d96c45dc6bfb6a410a5e4d44de9627f7787592e8
Jose Laurito <jigolaurito93@yahoo.com>
2023-07-31 01:20:34 -0500

    set up while loop to ask to guess number

--- game.py
+++ game.py
@@ -11,6 +11,9 @@ from guess_game_logo import logo
 # Print the Logo
 # print(logo)
 
+# Variables
+attempts = 10
+
 # Welcome message
 print('Welcome to the Number Guessing Game!')
 # Guess number from 1 - 100
@@ -23,9 +26,39 @@ print(f'Pssst, the correct answer is {answer}')
 # Ask for difficulty level
 difficulty = input(f"Choose a difficulty. Type 'easy' or 'hard':\n")
 # easy is 10 attempts, hard is 5 attempts
+if difficulty == 'easy':
+    attempts = 10
+elif difficulty == 'hard':
+    attempts = 5
 
-# Print how many attempts available
-# Ask to make a guess
+
+
+while attempts > 0:
+      # Print how many attempts available
+      print(f"You have {attempts} attempts remaining to guess the number.")
+
+      # Ask to make a guess
+      guess = int(input(f"Make a guess: "))
+      if guess != answer and attempts == 1:
+           if guess > answer:
+                attempts -= 1
+                print(f"Too high.")
+                print(f"You've run out of guesses, you lose.")
+           elif guess < answer:
+                attempts -= 1
+                print(f"Too low.")
+                print(f"You've run out of guesses, you lose.")
+      elif guess != answer:
+           if guess > answer:
+                print(f"Too high.\nGuess again.")
+                attempts -= 1
+           elif guess < answer:
+                print(f"Too low.\nGuess again.")
+                attempts -= 1
+
+                
+
+    
 
 # Guess again
 # Show how many attempts left
