90509872cc40f6334604019577b293ff25f34240
Jose Laurito <jigolaurito93@yahoo.com>
2023-07-31 00:34:07 -0500

    set up rules for the game

--- game.py
+++ game.py
@@ -1,1 +1,26 @@
-print("Hello world!")
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+# Print the Logo
+# Welcome message
+# Guess numhber from 1 - 100
+# Ask for difficulty level
+# easy is 10 attempts, hard is 5 attempts
+
+# Print how many attempts available
+# Ask to make a guess
+
+# Guess again
+# Show how many attempts left
+# Ask to make a guess again
+
+
+# If right, print YOU GOT IT, THE ANSWER WAS {answer}
+# If wrong, print You've run out of guesses, you lose.
